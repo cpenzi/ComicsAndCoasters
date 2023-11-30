9cfa7f992b06bcd252b1c1b84dae3a72c89d950a
Christopher Penzi <penzichr@gmail.com>
2023-11-29 19:36:35 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { BrowserRouter as Router, Switch, 
-  Route, Redirect, BrowserRouter,Routes, HashRouter} from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React from 'react';
-import './Styles/HeaderStyle.css'
-import Header from './Layout/Header'
+import './Styles/HeaderStyle.css';
+import Header from './Layout/Header';
 import HomePage from './Pages/HomePage';
-import Footer from './Layout/Footer'
+import Footer from './Layout/Footer';
 import MenuPage from './Pages/MenuPage';
 import AboutUsPage from "./Pages/AboutUsPage";
 import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <HashRouter basename="/">
+    <Router>
       <div className="App">
         <Header />
         <Routes>
+          {/* Set ComicsAndCoaster as the start page */}
           <Route path="/" element={<HomePage />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/aboutUs" element={<AboutUsPage />} />
           <Route path="/landing" element={<LandingPage />} />
+          {/* Redirect unknown routes to the home page */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
-    </HashRouter>
+    </Router>
   );
 }
 
